1ba34553bef3d17b35fbbd4a8302872cc098377b
Soumia08 <104898751+Soumia08@users.noreply.github.com>
2023-04-11 14:52:52 +0200

    Add files via upload

---
+++ src/component/AllHeader.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import HeaderBottom from "./HeaderBottom";
+
+function AllHeader() {
+ 
+       return (
+    <div>
+      <Header />
+      <HeaderBottom />
+      </div>
+    );
+  }
+  
+  export default AllHeader;
